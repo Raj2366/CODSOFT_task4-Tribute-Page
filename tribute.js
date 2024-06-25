cf8022ed9aae1e9f4f387821f20ba8e2b2c807ff
Rajuhalder <rajshilp2302@gmail.com>
2024-06-25 18:49:24 +0530

    First commit

---
+++ tribute.js
@@ -0,0 +1,10 @@
+pic = document.querySelector("#pic");
+
+window.onload = () => {
+    pic.style.transition = "all 15s ease-in-out";
+    pic.style.transform= 
+        pic.style.opacity = 1;
+    
+    
+    
+}
